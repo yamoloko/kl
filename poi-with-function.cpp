4f9ef61da7dac3719d65aa8bd2bc2f6eff9a8836
Anomalybay <sheributt094@gmail.com>
2025-05-15 08:18:44 -0700

    poi-with-function.cpp

--- poi-with-function.cpp
+++ poi-with-function.cpp
@@ -23,6 +23,10 @@ cout<<fun(poin,si)<<endl;
 
 printf("The size of the array is: %d\n", si);
 printf("They told me coding was fun. They lied. It's just Ctrl+C, Ctrl+V and tears.\n");
+printf("Why do programmers prefer dark mode? Because light attracts bugs!\n");
+printf("Why do Java developers wear glasses? Because they don't see sharp!\n");
+
+ 
 
 
     return 0;
