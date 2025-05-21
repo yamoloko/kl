0f15a00ef0b3774dac5b712b877ccaf95f13a516
Anomalybay <sheributt094@gmail.com>
2025-05-21 22:35:13 +0500

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -1,46 +1,23 @@
 #include<iostream>
-#include<fstream>
-#include<string>
 using namespace std;
-
-int  main() {
-string fileext , filename ;
-char lin[100];
-int a = 0;
-cout << "Enter the name of file (without extension): " << endl;
-cin >> filename;
-fileext = filename + ".txt";
-
-ifstream fil(fileext);
- 
-if(!fil){
-    cout << "File not found!" << endl;
-    return 1;
+int get(){
+    int a ;
+    cout<<"Enter the size if array:"<<endl;
+    cin>>a;
+    return a;
 }
- 
-//first method to use char[]
-while(fil.getline(lin , 30)){
-    cout << lin << endl ;
-    string lineStr(lin);// Convert char array to string
-    // Check each character in the line for vowels If lineStr = "Hello", the loop runs 5 times: c = 'H' c = 'e' c = 'l' c = 'l' c = 'o'
-    for (char c : lineStr) {//used to check each character in the string
-        //second way to read character 
-        // for(int i=0; i<str.length(); i++){
-        //     char c = str[i];
-        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
-            a++; // Increment the vowel count
-        }
-    }
+main(){
+
+int arr[] = {1,2,3,4,5};
+int size = get();
+int *ptr = arr;
+int temp = 0;
+
+for(int i=0; i<size; i++){
+temp += *ptr;
+ptr++;
 }
 
+cout<<temp;
 
-
- fil.close();
-
- 
-
-cout << a;
-
- return 0;
 }
--- 3.cpp
+++ 3.cpp
@@ -1,65 +1,37 @@
 #include<iostream>
 #include<fstream>
-#include<cstring>
-
+#include<cctype>
 using namespace std;
-int main(){
-string tyu;
-char ch[100];
-cout<<"Enter a character: "<<endl;
-cin>>tyu;
-
-ofstream fout("temp.txt");
-if (!fout) {
-    cout<< "Error opening file." << endl;
-     return 0;
+//& to get the original value not copy
+void count(ifstream &file , int &vowel , int &consonant , int &digit , int &special){
+    char ch;
+    vowel=consonant=digit=special=0;
+    while(file.get(ch)){
+        if(isalpha(ch)){
+            if(ch=='a' || ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                vowel++;
+            }
+            else{
+                consonant++;
+            }
+        }
+        else if(isdigit(ch)){
+            digit++;
+        }
+        else if (!isspace(ch)){
+            special++;
+        }
+    }
 }
-fout << tyu ;
-cout<<tyu;
-fout.close();
+main(){
+ifstream fil("ppp.txt");
+int vowels , consonants , digit , special;
+count(fil,vowels,consonants,digit,special);
 
-ifstream fin("temp.txt");
+cout<<"No of vowels: "<<vowels<<endl;
+cout<<"No of consonants: "<<consonants<<endl;
+cout<<"No of digits: "<<digit<<endl;
+cout<<"Special characters: "<<special<<endl;
 
-if (!fin) {
-    cerr << "Error opening file." << endl;
-    return 1;
+fil.close();
 }
-fin >> ch;
-cout<<ch<<endl;
-// string content((istreambuf_iterator<char>(fin)) , (istreambuf_iterator<char>())); //only used in ifstream 
-fin.close();
-
-int cha = strlen(ch);//check length of string
-cout<<cha<<endl;
-
-//first way
-for(int i=0; i<cha; i++){
-    if(ch[i] != ch[cha-1-i]){
-        cout<<"The string is not a palindrome."<<endl;
-        break;
-    }
-    else{
-        cout<<"The string is a palindrome."<<endl;
-        break;
-    }
-    }
-
-//second way    
-// bool isPal = true;
-// for(int i=0; i<cha; i++){
-//     if(ch[i] != ch[cha-1-i]){//check the first half and the second half of the string
-//         isPal = false;
-//         break;
-//     }
-// }
-
-// if(isPal){
-//     cout<<"The string is a palindrome."<<endl;
-// }
-// else{
-//     cout<<"The string is not a palindrome."<<endl;
-// }
-// cout<<cha<<endl;
-
-return 0;
-    }
